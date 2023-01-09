4c82aa56416c013d2f1cefa6b0a9286c5eb7a7e0
Keks <keks@htmlacademy.ru>
2023-01-09 12:32:41 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/open-menu.js
@@ -0,0 +1,24 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-header__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+navToggle.addEventListener('click', function () {
+  if (navToggle.classList.contains('main-header__toggle--closed')) {
+    navToggle.classList.remove('main-header__toggle--closed');
+    navToggle.classList.add('main-header__toggle--opened');
+  } else {
+    navToggle.classList.add('main-header__toggle--closed');
+    navToggle.classList.remove('main-header__toggle--opened');
+  }
+});
